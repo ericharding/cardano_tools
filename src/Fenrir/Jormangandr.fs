480ac226ce5a2c7759c6d2f8a5ac6675458214f2
Eric Harding <eric.harding@bluetradesys.com>
2020-01-22 22:33:36 -0500

    Giving up on auto generated code. Lets write it.
    Co-authored-by: Oqulent <us@oqulent.com>

--- src/Fenrir/Jormangandr.fs
+++ src/Fenrir/Jormangandr.fs
@@ -1,5 +1,50 @@
 module Jormangandr
 
-open Generated
+open System
+open System.Net.Http
+open Thoth.Json.Net
+open Newtonsoft.Json
 
-// 
+
+type Connection = private {
+  baseUri: Uri
+  client : HttpClient
+}
+ with 
+ interface IDisposable with
+   member this.Dispose() = this.client.Dispose()
+
+type Diagnostics = {
+  open_file_limit : uint32
+  cpu_usage_limit : uint64
+}
+
+let connect baseUri =
+  { baseUri = Uri(baseUri, UriKind.Absolute) 
+    client = new HttpClient() }
+
+let private get<'a> (c:Connection) (method:Uri) decoder =
+  use request = new HttpRequestMessage()
+  request.Method <- HttpMethod("GET")
+  request.Headers.Accept.Add(Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"))
+  request.RequestUri <- new Uri(c.baseUri, method)
+  async {
+    let! response = 
+      c.client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+      |> Async.AwaitTask
+    match int response.StatusCode with
+    | 200 -> 
+      let! data = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+      return decoder data
+    | _ -> 
+      return Error (sprintf "Http error %A" response.StatusCode)
+  }
+
+let private extraCoders = Extra.empty |> Extra.withInt64
+let private diagDecoder = Decode.Auto.generateDecoderCached<Diagnostics>(extra = extraCoders)
+
+let getDiagnostic (c:Connection) =
+  get c (Uri("/api/v0/diagnostic", UriKind.Relative)) (JsonConvert.DeserializeObject<Diagnostics> >> Ok)
+
+// let getNodeStats (c:Connection) =
+//   c.client.StatsAllAsync() |> Async.AwaitTask
--- src/Fenrir/Program.fs
+++ src/Fenrir/Program.fs
@@ -1,7 +1,8 @@
 ﻿// A wolf to watch the snake
 
-open Jormangandr
 open Argu
+
+let oculent_pool = "b628a5f5e987b03151c232b80d59a17e214e7105258c5bf8fc734a55ceb3e8d5"
 
 type Arguments =
   | Url of string
@@ -12,5 +13,16 @@ type Arguments =
               | Url _ -> "Url to connect to"
 
 
-printfn "hello world"
+
+let uri = "http://cerberus:57701"
+let c = Jormangandr.connect(uri)
+
+let stats = 
+  Jormangandr.getDiagnostic c
+  |> Async.RunSynchronously
+
+match stats with
+| Ok s -> printfn "%A" s
+| Error e -> printfn "%A" e
+// printfn "%A" stats
 
