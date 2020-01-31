f8c99d42202e9ce8d2f14687e5777d3e308ce5fc
Umed <58947306+Oqulent@users.noreply.github.com>
2020-01-31 07:53:47 -0500

    added block performance module
    The node should produce blocks in addition to being online.

--- src/Fenrir/Tools/NodeRunner.fs
+++ src/Fenrir/Tools/NodeRunner.fs
@@ -1,14 +1,14 @@
 module NodeRunner
-//Stuck Notifier:
+//A. Stuck Notifier:
 //Threshold time=300s
-//1.  Poll jormungandr node for latest block tip every 5 sec. (+ read logs for key words "stuck notifier")
-//2.  If there the tip does not progress after 300s:
+//1. Poll jormungandr node for latest block tip every 5 sec. (+ read logs for errors i.e. "stuck notifier")
+//2. If there the tip does not progress after 300s:
       // check external sources for the tip (i) pooltool.io (ii) shelleyexplorer for the tip:
-              // if the tips match--continue checking the tip(goto 1), if not:
+              // if the tips match--continue checking the tip (goto 1), if not:
               // send and email/text to operator with chain progress  error + time. 
               // restart the node. goto 1 [loop] 
               
-//Strategic Node Restart:
+//B. Strategic Node Restart:
 //Get leader schedule at 2.14PM every day.
 //Create a restart schedule for the node: 20 min before each block creation time
 //When the nodes hits the designated time: (i) restart the node, monitor chain for 4 min for new blocks. 
@@ -17,12 +17,24 @@ module NodeRunner
          //if node catches up, send a success! alert to node operator
     //else restart the node--send an alert to operator   
 
-//Keep Node Alive:
+//C. Keep Node Alive:
 //1. Check if node is running (every 30 seconds or every 10 seconds if block creation < 20 min):
       //if not running-->restart
-           //if running-->check if bootstrapping (set timer)
-                //if bootstrapping > 5 min.--> send alert to operator
+           //if running-->check stats--> (set timer)
+                //if checking block0 or bootstrapping > 5 min.--> send alert to operator --> restart
                 //else goto 1
+                
+//D. Block Performance
+//Call leader logs --> count the number of blocks to be created from schedule
+  //if block creation error--> notify operator 
+//After each block creation event-->check if block hash has decendants (this confirms someone built upon our tip) 
+   //keep track of blocks built on forks 
+//Calculate the stats: 
+//--wake up time vs. scheduled time
+//--block completion time vs. wakeup vs. schedule
+//--network propagation time (how long it took the network to send the block info back to us as a new tip)
+//--blocks built on forks vs healthy blocks.
+
 
 let run (cmd:string list) =
   ()
