2df138940a00ca383bd5b3cf3ea798b442da24c8
Oqulent <us@oqulent.com>
2020-02-29 21:44:32 -0500

    this one compiles

--- src/Fenrir/Cron.fs
+++ src/Fenrir/Cron.fs
@@ -7,19 +7,15 @@ type Schedule = {
   period : TimeSpan
   action : unit->unit
   deadline : DateTime
-  // state : 'a
 }
-
 
 let schedule period action initialState = 
   { id = Guid.NewGuid()
     period = period
     action = action
-    deadline = DateTime.MinValue 
-    state = initialState }
+    deadline = DateTime.MinValue }
 
-let rec run (schedule:Schedule<'a> list) =
-  // printfn "%A" schedule
+let rec run (schedule:Schedule list) =
   schedule
   |> List.sortBy (fun s -> s.deadline)
   |> List.tryHead
@@ -27,15 +23,15 @@ let rec run (schedule:Schedule<'a> list) =
   | None -> ()
   | Some head -> 
     Threading.Thread.Sleep (max (head.deadline - DateTime.Now) (TimeSpan.Zero))
-    let state' = head.action head.state
     run (
-      { head with state = state'; deadline = DateTime.Now + head.period } :: 
+      { head with deadline = DateTime.Now + head.period } :: 
       (List.filter (fun i -> head.id <> i.id) schedule))
 
 type Count = {
   counter : int
 }
-run [
-  schedule (TimeSpan.FromSeconds 5.) (fun t -> printfn "count = %i" t.counter; { t with counter = t.counter + 1}) ({ counter = 0 })
-]
+
+// run [
+//   schedule (TimeSpan.FromSeconds 5.) (fun () -> printfn "count = %i" t.counter; { t with counter = t.counter + 1}) ({ counter = 0 })
+// ]
 
