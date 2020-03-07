708fe6fe7497a61229178098ff16186ad88d3a87
Eric Harding <eric.harding@bluetradesys.com>
2020-03-06 23:28:32 -0500

    lower case fenrir published executable

--- src/Fenrir/publish.cmd
+++ src/Fenrir/publish.cmd
@@ -1,2 +1,2 @@
-@dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
+:: @dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
 @dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
